18ae722dc8b88a9320ddd35b5d003a62d2c0b99e
shivam-maharshi <shivam.maharshi@gmail.com>
2017-02-08 19:57:54 -0500

    Added basic skeleton for Lift.

---
+++ lift/src/main/java/com/lift/service/Service.java
@@ -0,0 +1,17 @@
+package com.lift.service;
+
+import java.util.List;
+
+/**
+ * All classes that perform the allocation of an {@link Entity} must implement
+ * this interface.
+ * 
+ * @author shivam.maharshi
+ */
+public class Service {
+
+  public static void main(String[] args) {
+	System.out.println("Hello");  
+  }
+
+}
